5ce5f723dac7db79d710541135bac93b183a2092
Satya <SatyaPawanK@outlook.com>
2019-03-30 18:44:55 +0530

    Add XCHG.

--- mem.h
+++ mem.h
@@ -5,6 +5,7 @@
  *        b) MOV
  *        c) NEG
  *        d) NOP
+ *        e) XCHG
  */
 
 #ifndef _ASE_MEM_H_
@@ -17,5 +18,6 @@ int hlt  (glob_t *glob, char *buf, unsigned long size);
 int move (glob_t *glob, char *buf, unsigned long size);
 int neg  (glob_t *glob, char *buf, unsigned long size);
 int nop  (glob_t *glob, char *buf, unsigned long size);
+int xchg (glob_t *glob, char *buf, unsigned long size);
 
 #endif
