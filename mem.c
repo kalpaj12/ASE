7259d688e640578c438770c833d2e8ad03cd9953
Satya <SatyaPawanK@outlook.com>
2019-03-31 17:33:34 +0530

    Code refinements
    1) HLT returns -1 instead of 1 (to differentiate between err and stop).
    2) Added xchg().
    3) move() has been re-written to:
    	a) Allow moving to xH and xL (x = A, B, C, D).
    	b) If user does not specify "H" postfix to operand, value is converted to hex.

--- mem.c
+++ mem.c
@@ -5,6 +5,7 @@
  *        b) MOV
  *        c) NEG
  *        d) NOP
+ *        e) XCHG
  */ 
 
 #include <assert.h>
@@ -24,7 +25,7 @@
  */ 
 int hlt(glob_t *glob, char *buf, unsigned long size) {
 	/* tengine intercepts this return value and sends to main. */
-	return 1;
+	return -1;
 }
 
 /**
@@ -35,85 +36,74 @@ int hlt(glob_t *glob, char *buf, unsigned long size) {
  * @return: int  - 0 if fail, 1 if success.
  */ 
 int move(glob_t *glob, char *buf, unsigned long size) {
-	assert(glob && glob->registers);
 
+	char val[1024];
 	char *src  = glob->tokens[2];
 	char *dest = glob->tokens[1];
 
-	if (!is_valid_op(dest) || !is_valid_op(src)) {
-		fprintf(stderr, "Invalid operands for MOV: [%s] [%s]\n", dest, src);
-		return 0;
-	}
-
 	if (!is_loc_reg(dest)) {
-		fprintf(stderr, "TErr: Dest must be register.\n");
+		fprintf(stderr, "MOV: Destination must be a register.\n");
 		return 0;
 	}
 
-	char temp[1024] = { 0 };
-	char *back = &src[strlen(src) - 1];
+	char *last = &src[strlen(src) - 1];
+	if (!isdigit(*last)) {
+		*last = toupper(*last);
+	}
 
-	/* Immediate addressing mode. */
-	if (*back == HEX_FS) {
-		switch (get_reg_size(dest)) {
-		case 8: {
-			if (strlen(src) > 3) {
-				fprintf(stderr, "Data too large for 8 bit reg [%s].\n", dest);
-				return 0;
-			}
+	if (is_loc_reg(src)) {
+		int dest_sz = get_reg_size(dest);
+		int src_sz  = get_reg_size(src);
 
-			break;
-		}
-
-		case 16: {
-			if (strlen(src) > 5) {
-				fprintf(stderr, "Data too large for 16 bit reg [%s].\n", dest);
-				return 0;
-			}
-
-			break;
-		}
-		}
-
-		/* Clip HEX_FS */
-		*back = '\0';
-		for (int i = 0; i < strlen(src); i++) {
-			if (isalpha(src[i])) {
-				if (src[i] > 'F') {
-					fprintf(stderr, "Invalid char in hex [%c].\n", src[i]);
-					return 0;
-				}
-			}
-		}
-
-		memcpy(temp, src, sizeof(temp));
-	/* Register addressing mode. */
-	} else if (is_loc_reg(src)) {
-		int r1_sz = get_reg_size(src);
-		int r2_sz = get_reg_size(dest);
-
-		if (r1_sz != r2_sz) {
+		if (dest_sz != src_sz) {
 			fprintf(stderr, "MOV: Both registers must be of same size.\n");
 			return 0;
 		}
 
-		if (!get_reg_val(glob, src, temp, sizeof(temp))) {
-			fprintf(stderr, "Could not fetch reg [%s] value.\n", src);
+		get_reg_val(glob, src, val, sizeof(val));
+		goto set;
+	}
+
+	switch (*last) {
+	case 'H': {
+		*last = '\0';
+		if (!is_valid_hex(src)) {
+			fprintf(stderr, "MOV: Invalid value [%s].\n", src);
+			return 0;	
+		}
+
+		strcpy(val, src);
+		break;
+	}
+
+	default: {
+		if (isalpha(*last)) {
+			fprintf(stderr, "MOV: Invalid specifier [%c].\n", *last);
 			return 0;
 		}
-	} else {
-		fprintf(stderr, "MOV: Invalid data specified.\n");
-		return 0;
+
+		int k_len = strlen(src);
+		if (k_len > ((get_reg_size(dest) == 16) ? 4 : 2)) {
+			fprintf(stderr, "MOV: Operand too large for %s.\n", dest);
+			return 0;
+		}
+
+		char *ptr = src;
+		while (*ptr) {
+			if (!isalnum(*ptr++)) {
+				fprintf(stderr, "MOV: Invalid value [%s].\n", src);
+				return 0;
+			}
+		}
+
+		sprintf(val, "%x", (unsigned int)strtol(src, NULL, 0));
+		break;
+	}
 	}
 
-	if (strlen(temp) == 0) {
-		fprintf(stderr, "Warning: Setting empty value. [%s] -> [%s]\n",
-		        temp, dest);
-	}
-
-	/* Set the destination register value. */
-	if (!set_reg_val(glob, dest, temp)) {
-		fprintf(stderr, "Could not set reg [%s] value.\n", dest);
+	set:
+	if (!set_reg_val(glob, dest, val)) {
+		fprintf(stderr, "MOV: Could not set value.\n");
 		return 0;
 	}
 
@@ -163,4 +153,33 @@ int neg(glob_t *glob, char *buf, unsigned long size) {
  */  
 int nop(glob_t *glob, char *buf, unsigned long size) {
 	return 1;
+}
+
+/**
+ * @desc  : Implements the XCHG instruction.
+ * @param : glob -
+ *          buf  - unused
+ *          size - unused
+ * @return: int  - 0 if fail, 1 if success.
+ * 
+ */
+int xchg(glob_t *glob, char *buf, unsigned long size) {
+
+	char *d_ptr = get_reg_ptr(glob, glob->tokens[1]);
+	char *s_ptr = get_reg_ptr(glob, glob->tokens[2]);
+
+	if (d_ptr && s_ptr) {
+		char *t = malloc(strlen(d_ptr) + 1);
+		if (!t) {
+			return 0;
+		}
+
+		strcpy(t, d_ptr);
+		strcpy(d_ptr, s_ptr);
+		strcpy(s_ptr, t);
+		free(t);
+		return 1;
+	}
+
+	return 0;
 }
