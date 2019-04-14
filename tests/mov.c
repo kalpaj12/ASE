60c5d6d232fe87c8864603f42f54e76d22af1cd3
Satya <SatyaPawanK@outlook.com>
2019-04-14 19:23:09 +0530

    Added tests for move - move to mem loc.

--- tests/mov.c
+++ tests/mov.c
@@ -25,6 +25,9 @@ int main(void) {
 	char l_2[] = "MOV BX, AX";
 	char l_3[] = "MOV CX, DX";
 	char l_4[] = "MOV DL, 34H";
+	char l_5[] = "MOV [1234H], 34H";
+	char l_6[] = "MOV [1245], 34H";
+	char l_7[] = "MOV [12], 34H";
 
 	parse_line(glob, l_1);
 	move(glob, NULL, 0);
@@ -54,6 +57,27 @@ int main(void) {
 		return 1;
 	}
 
+	int i = 0;
+	int arr[] = {12, 1234, 1245};
+
+	parse_line(glob, l_5);
+	move(glob, NULL, 0);
+
+	parse_line(glob, l_6);
+	move(glob, NULL, 0);
+
+	parse_line(glob, l_7);
+	move(glob, NULL, 0);
+
+	mem_nodes_t *node = glob->mem->head;
+	while (node) {
+		assert(node->seg == 0);
+		assert(node->offset == arr[i]);
+		node = node->next;
+		i++;
+	}
+
+	assert(i);
 	fclose(fd);
 	return 0;
 }
