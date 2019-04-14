a72cc1e1bf032c4783b49805e6a6786e45e15bb7
Satya <SatyaPawanK@outlook.com>
2019-04-14 14:03:40 +0530

    Code improvements
    1. Moved local headers to display.h
    2. Simplified the code that prints the stack contents.

--- display.c
+++ display.c
@@ -10,23 +10,6 @@
 #include <string.h>
 
 #include "display.h"
-#include "parse.h"
-
-/**
- * @desc  : Display specified register value.
- * @param : reg - Register's value needed to display.
- *          val - Value of register.
- * @return: void
- */
-void display_reg_val(char *reg, char *val) {
-	char reg_[1024] = {0};
-	memcpy(reg_, reg, sizeof(reg_));
-
-	char *back = &reg_[strlen(reg_) - 1];
-	const int klen = strlen(val);
-	*back = (klen <= 2 && klen >= 1) ? 'L' : 'X';
-	printf("[%s] - [%s]\n", reg_, val);
-}
 
 /**
  * @desc  : Display data from memory, registers and stack.
@@ -37,31 +20,30 @@ void display(glob_t *glob) {
 	assert(glob);
 
 	if (glob->flags) {
-		printf("Flags :\n");
-		printf("[Flag]:[Bit value] - [CF]:[%d]\n", glob->flags->cf);
-		printf("[Flag]:[Bit value] - [DF]:[%d]\n", glob->flags->df);
-		printf("[Flag]:[Bit value] - [IF]:[%d]\n", glob->flags->iif);
-		printf("[Flag]:[Bit value] - [PF]:[%d]\n", glob->flags->pf);
-		printf("[Flag]:[Bit value] - [ZF]:[%d]\n\n", glob->flags->zf);
+		printf("Flags:\n");
+		printf("[CF]:[%d]\n",   glob->flags->cf);
+		printf("[DF]:[%d]\n",   glob->flags->df);
+		printf("[IF]:[%d]\n",   glob->flags->iif);
+		printf("[PF]:[%d]\n",   glob->flags->pf);
+		printf("[ZF]:[%d]\n\n", glob->flags->zf);
 	}
 
 	if (glob->registers) {
 		printf("Register:\n");
-		display_reg_val(REG_AX, glob->registers->ax);
-		display_reg_val(REG_BX, glob->registers->bx);
-		display_reg_val(REG_CX, glob->registers->cx);
-		display_reg_val(REG_DX, glob->registers->dx);
+		printf("[AX]:[%s]\n",   glob->registers->ax);
+		printf("[BX]:[%s]\n",   glob->registers->bx);
+		printf("[CX]:[%s]\n",   glob->registers->cx);
+		printf("[DX]:[%s]\n\n", glob->registers->dx);
 	}
 
 	if (glob->stack) {
-		int idx = glob->stack->top;
-		idx > -1 ? printf("\nStack :\n") : idx;
+		printf("Stack:\n");
+		char **ptr = &glob->stack->arr[0];
 
-		for (int i = 0; i <= idx; i++) {
-			printf("[Addr]:[Value] - [%p]:[%s]\n", (void *)&glob->stack->arr[i],
-			                            (char *)glob->stack->arr[i]);
-			free(glob->stack->arr[i]);
-			i == idx ? printf("\nNote: Stack memory cleared.\n") : 0;
+		while (*ptr) {
+			char **t = ptr;
+			printf("[%p]:[%s]\n", (void*)&*t, *ptr++);
+			free(*t);
 		}
 	}
 }
