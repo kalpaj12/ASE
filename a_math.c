d5d35adfdfd964e0189c01e143b44e22697bf741
Satya <SatyaPawanK@outlook.com>
2019-05-04 03:06:44 +0530

    Code refinements
    1. Improvements to math_op() to support multiple addressing modes.

--- a_math.c
+++ a_math.c
@@ -25,87 +25,98 @@
 int math_op(glob_t *glob, char *buf, unsigned long size) {
 
 	if (!glob) {
-		fprintf(stderr, "add(): glob - null\n");
+		fprintf(stderr, "math_op(): glob - null\n");
 		return 0;
 	}
 
-	char val[1024];
-	char *instr = glob->tokens[0];
-	char *dest  = glob->tokens[1];
-	char *src   = glob->tokens[2];
+	/* Let AX (accumulator) be the default destination */
+	char *ptr = get_reg_ptr(glob, REG_AX);
 
-	/* Accumulator is the default register for MUL & DIV */
-	if (!strcmp(instr, MUL) || !strcmp(instr, DIV)) {
-		dest = get_reg_ptr(glob, REG_AX);
-	}
+	char *inst = glob->tokens[0];
+	char *dest = glob->tokens[1];
+	char *src_ = glob->tokens[2];
+	char dval[BUF_SZ], sval[BUF_SZ], res[BUF_SZ];
 
-	/* For now */
-	assert(is_loc_reg(dest));
-	unsigned long d_val = 0, s_val = 0, result = 0;
-
-	if (!reg_to_ul(glob, dest, &d_val)) {
-		fprintf(stderr, "add(): Could not fetch dest value.\n");
-		return 0;
-	}
-
-	if (is_loc_reg(src)) {
-		(void)reg_to_ul(glob, src, &s_val);
-		goto set;
-	}
-
-	/* Immediate addressing mode */
-	int base = 0;
-	char *last = &src[strlen(src) - 1];
-
-	switch (*last) {
-	case HEX_FS: {
-		*last = '\0';
-		if (!is_valid_hex(src)) {
-			fprintf(stderr, "add(): [%s] is not valid hex.\n", src);
+	if (strcmp(inst, DIV) == 0 || strcmp(inst, MUL) == 0) {
+		if (glob->n_op != 1) {
+			char *z = glob->n_op > 1 ? "excess" : "missing";
+			fprintf(stderr, "math_op(): %s - %s instructions.\n", inst, z);
 			return 0;
 		}
 
-		base = 16;
-		break;
+		/**
+		 * DIV & MUL take only 1 operand.
+		 * The default and the destination operand is AX (accumulator).
+		 */
+		dest = REG_AX;
+		src_ = glob->tokens[1];
 	}
 
-	default: {
-		char *ptr = src;
-		while (*ptr) {
-			if (!isdigit(*ptr++)) {
-				fprintf(stderr, "add(): [%s] is invalid value.\n", src);
+	int ret1 = get_op_val(glob, dest, dval, sizeof(dval));
+	int ret2 = get_op_val(glob, src_, sval, sizeof(sval));
+
+	if (ret1 ^ ret2) {
+		return 0;
+	}
+
+	int c_dval = (int)strtol(dval, NULL, 16);
+	int c_sval = (int)strtol(sval, NULL, 16);
+
+	if (strcmp(inst, ADD) == 0) {
+		sprintf(res, "%d", c_dval + c_sval);
+	} else if (strcmp(inst, SUB) == 0) {
+		/* Answer is 0. Set zero flag */
+		if (c_dval == c_sval) {
+			glob->flags->zf = 1;
+		}
+
+		sprintf(res, "%d", c_dval - c_sval);
+	} else if (strcmp(inst, MUL) == 0) {
+		sprintf(res, "%d", c_dval * c_sval);
+		goto set;
+	} else if (strcmp(inst, DIV) == 0) {
+		sprintf(res, "%d", c_dval / c_sval);
+		goto set;
+	} else if (strcmp(inst, CMP) == 0) {
+		if (c_dval < c_sval) {
+			glob->flags->cf = 1;
+		} else if (c_dval == c_sval) {
+			glob->flags->zf = 1;
+		}
+		return 1;
+	} else {
+		return 0;
+	}
+
+	/**
+	 * Set the destination pointer. 
+	 * This part is skipped if the default destination has been set to
+	 * AX (accumulator).
+	 */
+	if (is_op_addr(dest)) {
+		char addr[BUF_SZ];
+		memset(addr, 0, sizeof(addr));
+		memcpy(addr, &dest[1], strlen(dest) - 2);
+		
+		for (int i = 0; i < strlen(addr); i++) {
+			if (!isdigit(addr[i])) {
+				fprintf(stderr, "move(): invalid address [%s].\n", addr);
 				return 0;
 			}
 		}
 
-		base = 0;
-		break;			
-	}
+		int offset = (int)strtol(addr, NULL, 0);
+		ptr = add_to_mem(glob, 0, offset)->val;
+	} else if (is_op_reg(dest)) {
+		ptr = get_reg_ptr(glob, dest);
 	}
 
-	s_val = strtol(src, NULL, base);
-
-	set:
 	/**
-	 * The code till previous line is common for many instructions.
-	 * The next section here identifies the instruction that was called.
+	 * We skip to this part if AX (accumulator) has been decided as the
+	 * default destination.
 	 */
-	if (!strcmp(instr, ADD)) {
-		result = d_val + s_val;
-	} else if (!strcmp(instr, CMP)) {
-		if (d_val == s_val) {
-			glob->flags->zf = 1;
-		} else if (d_val < s_val) {
-			glob->flags->cf = 1;
-		}
+	set:
+	assert(ptr);
 
-		return 1;
-	} else {
-		fprintf(stderr, "math_op(): Unsupported operand [%s].\n", instr);
-		return 0;
-	}
-
-	sprintf(val, "%ld", result);
-	(void)set_reg_val(glob, dest, val, 1);
-	return 1;
+	return get_op_val(glob, res, ptr, BUF_SZ);
 }
