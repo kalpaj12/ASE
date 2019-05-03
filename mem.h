6642cbe599f9f0b74a55d039b4023b8393fbf4e4
Satya <SatyaPawanK@outlook.com>
2019-05-04 03:03:10 +0530

    Added unary(): DEC & INC.

--- mem.h
+++ mem.h
@@ -18,6 +18,7 @@ int hlt  (glob_t *glob, char *buf, unsigned long size);
 int move (glob_t *glob, char *buf, unsigned long size);
 int neg  (glob_t *glob, char *buf, unsigned long size);
 int nop  (glob_t *glob, char *buf, unsigned long size);
+int unary(glob_t *glob, char *buf, unsigned long size);
 int xchg (glob_t *glob, char *buf, unsigned long size);
 
 #endif
