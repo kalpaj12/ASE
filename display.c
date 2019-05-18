d420979bebf39bbab9467aaf4d988d7871f9860f
kalpaj12 <kalpaj.agrawalla01@gmail.com>
2019-05-18 11:41:54 +0530

    Code reformat

--- display.c
+++ display.c
@@ -34,6 +34,14 @@ void display(glob_t *glob, args_t p_args) {
 		return;
 	}
 
+	if (p_args.h) {
+		show_flags();
+	}
+
+	if (p_args.v) {
+		printf("Build : %d\nAuthor: Pawan Kartik\n", BUILD);
+	}
+	
 	if (p_args.f && glob->flags) {
 		printf("Flags:\n");
 		printf("[CF]:[%d]\n",   glob->flags->cf);
@@ -87,13 +95,5 @@ void display(glob_t *glob, args_t p_args) {
 			printf("[%d:%d] - [%s]\n", node->seg, node->offset, node->val);
 			node = node->next;
 		}
-	}
-
-	if (p_args.h) {
-		show_flags();
-	}
-
-	if (p_args.v) {
-		printf("Build : %d\nAuthor: Pawan Kartik\n", BUILD);
 	}
 }
